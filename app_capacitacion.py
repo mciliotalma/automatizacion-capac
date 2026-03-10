c7afee66f6f8c06664efe966db892d12a807dbf9
mciliotalma <miguel.cilio@talma.com.ec>
2026-03-10 09:03:16 -0500

    Update app_capacitacion.py

--- app_capacitacion.py
+++ app_capacitacion.py
@@ -97,7 +97,7 @@ st.write("")
 # --------------------------------------------------
 
 st.markdown("## 📄 Formato requerido del archivo")
-st.info("El archivo Excel debe tener exactamente esta estructura y debe contener la pestaña llamada: Portal Acumulado")
+st.info("El archivo Excel debe tener exactamente esta estructura y debe contener la pestaña llamada: Acumulado Portal")
 
 tabla_html = """
 <table style="border-collapse:collapse;width:100%;font-size:14px">
@@ -275,6 +275,7 @@ if uploaded_file:
         file_name="Capacitaciones_TALMA_Profesional.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
+
 
 
 
